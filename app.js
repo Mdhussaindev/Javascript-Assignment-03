ccc29468132c4e3f58565e1fd6cb304963ca9b32
Mdhussaindev <mdhussain.dev00@gmail.com>
2025-12-09 21:43:35 +0500

    update

---
+++ app.js
@@ -0,0 +1,30 @@
+// Q1
+var city = prompt ("Enter your city");
+if ( city === "Karachi") {
+    console.log(" Welcome tocity of lights");
+} else{
+    console.log ("bye");
+}
+// Q2
+var gender = prompt ("Enter your gender");
+if (city === "male") {
+    console.log(" Hello sir");
+}
+if  (gender === "female"){
+    console.log("Hello Ma'am");
+}
+// Q3
+var signalcolor = prompt("Enter your color");
+if (signalcolor === "Red"){
+    console.log("Must stop")
+}
+
+var signalcolor = prompt("Enter your color");
+if (signalcolor === "Yellow"){
+    console.log("Ready")
+}
+
+var signalcolor = prompt("Enter your color");
+if (signalcolor === "Green"){
+    console.log("Go")
+}
